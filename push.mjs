4c9847b141bb72b5b97fb495513be5a0664ae278
TransonQ <quanscheng@qq.com>
2023-06-14 13:15:25 +0800

    存档时间: 2023-6-14 13:15:25

--- push.mjs
+++ push.mjs
@@ -1,9 +1,38 @@
 #!/usr/bin/env zx
+// 时间
+const d = new Date()
+const year = d.getFullYear()
+const month = d.getMonth() + 1
+const day = d.getDate()
+const hour = d.getHours()
+const min = d.getMinutes()
+const sec = d.getSeconds()
+const time = ` 存档时间: ${year}-${month}-${day} ${hour}:${min}:${sec}`
 
-let msg = await question(chalk.cyan('commit and push -message: 👇 \n'))
+// 查看当前分支名
+let process = await spinner(
+  'checking current branch',
+  () => $`git branch --show-current`
+)
+const branch = process.stdout.trim()
 
-await $`git add .`
+// 提交
+if (branch !== 'master') {
+  console.log(chalk.red('Switch to the master branch \n'))
+} else {
+  let msg = await question(chalk.cyan('Enter the commit message: '))
 
-await $`git commit -m ${msg}`
+  await spinner('adding...', () => $`git add .`)
 
-await $`git push`
+  await spinner('committing...', () => $`git commit -m ${msg + time}`)
+
+  await spinner('pushing...', () => $`git push`)
+
+  console.log(`
+  ███████╗██╗   ██╗ ██████╗ ██████╗███████╗███████╗███████╗
+  ██╔════╝██║   ██║██╔════╝██╔════╝██╔════╝██╔════╝██╔════╝
+  ███████╗██║   ██║██║     ██║     █████╗  ███████╗███████╗
+  ╚════██║██║   ██║██║     ██║     ██╔══╝  ╚════██║╚════██║
+  ███████║╚██████╔╝╚██████╗╚██████╗███████╗███████║███████║
+  ╚══════╝ ╚═════╝  ╚═════╝ ╚═════╝╚══════╝╚══════╝╚══════╝`)
+}
